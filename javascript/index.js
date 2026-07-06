09c603a402f5e9c872cae985a1ccecbf2c2831b4
Amitkm997 <amitkm997@gmail.com>
2026-07-06 12:42:43 +0530

    javascript code index.js

---
+++ javascript/index.js
@@ -0,0 +1,51 @@
+
+
+// let var and const 
+
+// const name="Amit";
+
+// console.log(name);
+
+// var a=90; //
+
+// //declaration
+// var a; 
+
+// //initilization
+// a=90
+
+// in case of var we can redeclare the varible 
+// var b=89;
+// var b=90;
+
+// console.log(b);
+
+// in case of let we can redeclare the varible 
+// let a=67;
+// let a=90; //'a' has already been declared
+
+// console.log(a);
+
+
+// in case of let we can redeclare the varible 
+
+// const a=3.14;
+// const a=90;
+// console.log(a)
+
+// var a;
+// a=90
+// console.log(a)
+
+
+// let a;
+// a=90
+// console.log(a)
+
+// const a;
+// a=89;
+// console.log(a)
+
+// const pi=3.14
+
+// console.log(a)
