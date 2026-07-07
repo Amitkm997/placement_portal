9ac55664e78762c21012a7f28027da21a6ef0c1b
Amitkm997 <amitkm997@gmail.com>
2026-07-07 12:34:56 +0530

    js funtions

--- javascript/index.js
+++ javascript/index.js
@@ -49,3 +49,66 @@
 // const pi=3.14
 
 // console.log(a)
+
+// hoisting 
+// var a;
+// console.log(a); //ReferenceError: a is not defined
+
+
+// console.log(b); //Cannot access 'b' before initialization
+// let b;
+
+
+//function
+
+// a. how to declare function 
+// b how to call funtions
+
+// types of function 
+// 1. normal funtion 
+// 2. function expression 
+// 3. arrow function (important)
+// 4 Immidiatly invoke funtion expression (IIFE)
+
+// 1. normal funtion 
+
+// a. how to declare function 
+// function greet(name,profession){  // parameter
+//     console.log("Hi There! "+name +" I'm a "+profession);
+// }
+
+// // b how to call funtions // invoke 
+// greet("Amit","Trainer"); // arguments
+
+
+// 2. function expression 
+// a. how to declare function 
+// let greet=function(name){
+//     console.log("Hi There!")
+// }
+
+// // // b how to call funtions // invoke 
+// greet("amit");
+
+// 3. arrow function (important)
+// a. how to declare function
+// let greet=()=>{
+//     return "Hi there"
+// }
+
+// // // // b how to call funtions // invoke 
+// let output=greet();
+// console.log(output)
+
+// let greet=()=>"Hi threre";
+
+// let greet=name=>name;
+
+// console.log(greet("Amit"));
+
+// 4 Immidiatly invoke funtion expression (IIFE)
+(
+    function(){
+        console.log('Hi there')
+    }
+)();
---
+++ javascript/loops.js
@@ -0,0 +1,58 @@
+// conditional statment
+
+// if(condition){
+//     //if condition is true
+// }else{
+//     //else this will execute
+// }
+
+// let num=77;
+
+// if(num%2==0){
+//     console.log("this is an even no.")
+// }else if(num%2==1){
+//     console.log("this is odd no. ")
+// }else{
+//     console.log("this is invalid no. ")
+// }
+
+
+// WAP that prints foo if a no. is divisible by 3
+// and print baar is a no. is divisible by 5
+// and print foobaar if no. is divisible bt both
+
+
+// daksh
+// let a = 7;
+
+// if (a%3 == 0){
+//     if(a%5==0){
+//         console.log("foobar")
+//     }
+//     else{
+//         console.log("foo");
+//     }
+// }
+// else if(a%5==0){
+//     console.log("baar");
+// }
+
+// else{
+//     console.log("Not div by 3 and 5")
+// }
+
+
+//arshpreet
+// let number = 15;
+// if (number % 3 === 0 && number % 5 === 0) {
+//     console.log("foobaar");
+// }
+// else if (number % 3 === 0) {
+//     console.log("foo");
+// }
+// else if (number % 5 === 0) {
+//     console.log("baar");
+// }
+// else {
+//     console.log("invalid number");
+// }
